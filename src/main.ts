16c57c071fbdf80be8c34512936c72a8ed99f736
futuristicCode <baoumaramadoubocar@gmail.com>
2025-04-29 01:51:32 +0000

    Creation user,prisma resource && installation same modules (bcrypt,passport,jwt,prisma-client........)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await app.setGlobalPrefix('api');
+  await app.useGlobalPipes(new ValidationPipe({whitelist:true}));
+
+  const config = new DocumentBuilder()
+  .setTitle('The Shopping API')
+  .setDescription('The Shopping API DOC ')
+  .setVersion('1.0')
+  .addTag('Shopping Api')
+  .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
